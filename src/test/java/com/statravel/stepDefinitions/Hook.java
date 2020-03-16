33c6967ab655a96cd70e797e2d6009f362094f25
Adams, Kiranmai <kiranmai.adams@statravel.com>
2020-03-16 19:12:19 +0000

    updated webdriver code

--- src/test/java/com/statravel/stepDefinitions/Hook.java
+++ src/test/java/com/statravel/stepDefinitions/Hook.java
@@ -2,9 +2,9 @@ package com.statravel.stepDefinitions;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-
+import static io.github.bonigarcia.wdm.DriverManagerType.CHROME;
 import org.openqa.selenium.PageLoadStrategy;
-
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.ITestResult;
@@ -19,25 +19,35 @@ import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import gherkin.formatter.model.Result;
-
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.DriverManagerType;
+import io.github.bonigarcia.wdm.WebDriverManager;
 public class Hook extends BaseUtil {
 	 static ExtentReports extentReports;
 	 static ExtentHtmlReporter htmlReporter;
 	 static ITestResult iTestResult;
 
-	
+	// private static WebDriver driver;
 	
 	@Before
 	public void initializeTest(Scenario scenario) {
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/WebDrivers/chromedriver.exe");
-		ChromeOptions options = new ChromeOptions();
-		options.setExperimentalOption("useAutomationExtension", false);
+	//	System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/WebDrivers/chromedriver.exe");
+		//WebDriverManager.chromedriver().setup();
+		//System.setProperty("wdm.proxy", "http://genproxy.corp.amdocs.com:8080");
+		
+		DriverManagerType chrome = DriverManagerType.CHROME;
+    	ChromeDriverManager.getInstance(chrome).setup();
+    ChromeOptions options = new ChromeOptions();
+	options.setExperimentalOption("useAutomationExtension", false);
 		options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-		options.addArguments("start-maximized");
+		//options.addArguments("start-maximized");
 		//options.addArguments("--headless")
 		driver = new ChromeDriver(options);
+		/*
+		 * WebDriver driver = new ChromeDriver(); driver.manage().window().maximize();
+		 */
 		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
-
+System.out.println("opened driver");
 	}
 
 	@After
