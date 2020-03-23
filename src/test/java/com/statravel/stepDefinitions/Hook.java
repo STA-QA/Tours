f75c06d1f8a7cb4657e1d5f7c6ca4643fb222b32
Kiranmai Adams <kiranmai.adams@statravel.com>
2020-03-23 15:11:23 +0000

    Update Hook.java

--- src/test/java/com/statravel/stepDefinitions/Hook.java
+++ src/test/java/com/statravel/stepDefinitions/Hook.java
@@ -2,14 +2,14 @@ package com.statravel.stepDefinitions;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-import static io.github.bonigarcia.wdm.DriverManagerType.CHROME;
+import java.net.MalformedURLException;
 import org.openqa.selenium.PageLoadStrategy;
-import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.ITestResult;
 
-
+import static io.github.bonigarcia.wdm.DriverManagerType.CHROME;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.statravel.base.BaseUtil;
@@ -20,34 +20,46 @@ import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import gherkin.formatter.model.Result;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
-import io.github.bonigarcia.wdm.DriverManagerType;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.DriverManagerType;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.URL;
+import org.openqa.selenium.WebDriver;
+
 public class Hook extends BaseUtil {
-    
 	 static ExtentReports extentReports;
 	 static ExtentHtmlReporter htmlReporter;
 	 static ITestResult iTestResult;
-	 
-	// private static WebDriver driver;
+
+	
 	
 	@Before
 	public void initializeTest(Scenario scenario) {
+	   Capabilities chromeCapabilities = DesiredCapabilities.chrome();
+		try {
+			WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeCapabilities);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Driver error");
+			e.printStackTrace();
+		}
 	//	System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/WebDrivers/chromedriver.exe");
-		//WebDriverManager.chromedriver().setup();
-		//System.setProperty("wdm.proxy", "http://genproxy.corp.amdocs.com:8080");
-		DriverManagerType chrome = DriverManagerType.CHROME;
+		
+	//	System.setProperty("wdm.proxy", "http://genproxy.corp.amdocs.com:8080");
+ /*       WebDriverManager.chromedriver().config().setProperties("webdrivermanager.properties");
+    	DriverManagerType chrome = DriverManagerType.CHROME;
     	ChromeDriverManager.getInstance(chrome).setup();
-    ChromeOptions options = new ChromeOptions();
-	options.setExperimentalOption("useAutomationExtension", false);
+    	ChromeOptions options = new ChromeOptions();
+		options.setExperimentalOption("useAutomationExtension", false);
 		options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-		options.addArguments("start-maximized");
-		options.addArguments("--headless");
+		options.setBinary("/./bin/google-chrome");
+	//	options.addArguments("start-maximized");
+		//options.addArguments("--headless")
 		driver = new ChromeDriver(options);
-		/*
-		 * WebDriver driver = new ChromeDriver(); driver.manage().window().maximize();
-		 */
-		driver.manage().timeouts().implicitlyWait(180, TimeUnit.SECONDS);
-System.out.println("opened driver");
+		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);*/
+
 	}
 
 	@After
